2b5f73f2e62fb4e8e8eb58112a1793f0cd7ab1b0
pasindujy <pasindu.jay@outlook.com>
2024-04-20 17:45:28 +0200

    Improvements to index php

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>Hello World</p>'; ?>
+        <?php echo '<p>Hello World</p>';
+        if($URI = getenv('DBDataserviceURI'))
+        echo $URI;
+        ?>
     </body>
 </html>
